26ca284d5203555ccb852e99222c4a3a93bca2a8
leilimin <461083868@qq.com>
2022-07-24 16:24:28 +0800

    doc(C): 第一章

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+// 为何在此处声明,因为程序是由上至下执行的,如果不在此处声明,那执行至main()时就不知道print_hello()表示的含义
+// 1.声明函数原型
+void print_hello(); /* help print "hello world"*/
+
 int main() {
-    printf("Hello, World!\n");
+    // 2.函数调用
+    print_hello();
     return 0;
+}
+
+// 3.函数定义
+void print_hello(){
+    printf("Hello, World!\n");
 }
