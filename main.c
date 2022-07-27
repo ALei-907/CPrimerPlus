8c6b60e83ea6976317d969f7a96d94fad3b2e26b
leilimin <461083868@qq.com>
2022-07-27 23:17:23 +0800

    feat(C): 第6章
    1.wile,for,do while
    2.逗号表达式
    3.scan的输入缓冲区

--- main.c
+++ main.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-#include "string.h"
-#include "limits.h"
-
-#define DENSITY 62.4
-#define TEST "Hello Lewis Lei "
 
 // 为何在此处声明,因为程序是由上至下执行的,如果不在此处声明,那执行至main()时就不知道print_hello()表示的含义
 // 1.声明函数原型
@@ -17,13 +12,10 @@ int main() {
 
 // 3.函数定义
 void practice() {
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
 
-    int i;
-    char c;
-    scanf("%d %c", &i, &c);
-    printf("\n");
-    printf("i = %d\n", i);
-
-    printf("c = \"%c\"\n", c);
+    printf("%d,%d",a,b);
 
 }
---
+++ section6/section6.c
@@ -0,0 +1,63 @@
+//
+// Created by 雷立民 on 2022/7/27.
+//
+#include "stdio.h"
+
+void section6(void) {
+    /**
+     * input: 1 2 [Enter]
+     * output: 1,2
+     * 解释: scanf会缓存输入序列
+     */
+    int a1, b1;
+    scanf("%d", &a1);
+    scanf("%d", &b1);
+    printf("%d,%d", a1, b1);
+
+
+    long l1;
+    while (scanf("%ld", &l1) == 1) {
+        printf("%ld\n", l1);
+        printf("plz input a Long Number (q is quit):\n");
+    }
+
+
+    /**
+     * result:
+     * 2
+     * 1
+     * decipher:
+     * 1.逗号表达式会从左至又进行表达式求值,最后将最右侧的值作为 "赋值语句左侧" 的值
+     * 2.    a = 1, 2; => a=1; 2;
+     */
+    int a;
+    a = (1, 2);
+    printf("%d\n", a);
+    a = 1, 2;
+    printf("%d\n", a);
+
+    /**
+     * c += 2.0f / 6.0f = 1.333333
+     * c = (c + 2.0f) / 6.0f; = 0.500000
+     * c = c + (2.0f / 6.0f); =1.333333
+     * 结论就是: /的优先级比+=更高
+     */
+    float c = 1.0f;
+    c = c + (2.0f / 6.0f);
+    printf("%f", c);
+
+    while (scanf("%ld", &l1) == 1) {
+        printf("%ld\n", l1);
+        printf("plz input a Long Number (q is quit):\n");
+    }
+
+    /**
+     * do wile循环
+     */
+    do {
+        printf("%ld\n", l1);
+        printf("plz input a Long Number (q is quit):\n");
+    } while (scanf("%ld", &l1) == 1);
+
+
+}
