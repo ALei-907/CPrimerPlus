e30043e693a0bbd7f95c239d28b7ef0151d28530
leilimin <461083868@qq.com>
2022-07-29 16:26:20 +0800

    feat(C): 第10章
    1.一维数组的声明与初始化

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#include "stdlib.h"
+
+#define MONTHS 12
 
 // 为何在此处声明,因为程序是由上至下执行的,如果不在此处声明,那执行至main()时就不知道print_hello()表示的含义
 // 1.声明函数原型
-void practice(); /* help print "hello world"*/
+void practice(void); /* help print "hello world"*/
 
 int main() {
     // 2.函数调用
@@ -12,21 +13,11 @@ int main() {
 }
 
 // 3.函数定义
-void practice() {
-    int ch;
-    FILE *fp;
-    char fname[50]; /* 一个文件名 */
-    printf("请输入一个文件名.\n");
-    scanf("%s", fname);
-    fp = fopen(fname, "wt");
-    if (fp == NULL) {
-        printf("输出: 空文件! Bye.\n");
-        exit(1); /* 退出成勋 */
-    }
-    /*getc()从fp中获取一个字符*/
-    while ((ch = getc(fp)) != EOF) {
-        putchar(ch);
-    }
-    fclose(fp); /* 关闭文件 */
-    return;
+void practice(void) {
+    int staff[] = {1, [6]=7};
+    int stuff[] = {1, [6]=7, 8, 9};
+    printf("staff size = %zd\n", sizeof staff / sizeof staff[0]);
+    printf("stuff size = %zd\n", sizeof stuff / sizeof stuff[0]);
 }
+
+
---
+++ section10/section10.c
@@ -0,0 +1,72 @@
+//
+// Created by 雷立民 on 2022/7/29.
+//
+#include "stdio.h"
+
+#define MONTHS 12 /* 声明常量 */
+
+/**
+ * 1.数组禁止在初始化之后使用花括号的形式进行赋值
+ */
+void section10(void) {
+    /**
+     * 声明一个只读数组,这样days[]初始化之后,只可以被访问,而不能被修改
+     * * 如果对数组不进行初始化,那么读取对应下标的值为当前数组元素所在内存上存储的值
+     * * 如果对数组进行不完全初始化,那么会进行初始化的元素会被设置为默认值,eg int=0;
+     */
+    const int days[MONTHS] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    /* 通过初始化元素的项数来定义数组大小也是可以的 */
+    const int days2[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    for (int index = 0; index < MONTHS; index++) {
+        printf("MONTH %d has %d days\n", index + 1, days[index]);
+    }
+
+    /**
+     * 回忆一下 sizeof
+     * sizeof days2: 获取days所持有内存区域的总字节数
+     * sizeof days2[0]: 获取一个元素所持有的字节数
+     * sizeof days2/sizeof days2[0]: 数组大小
+     */
+    for (int index = 0; index < sizeof days2 / sizeof days2[0]; index++) {
+        printf("MONTH %d has %d days\n", index + 1, days2[index]);
+    }
+    /**
+     * daysSize= 48 bytes
+     * days2Size= 48 bytes
+     * days3Size= 48 bytes
+     */
+    const int days3[MONTHS];
+    printf("daysSize= %zd bytes\n", sizeof days);
+    printf("days2Size= %zd bytes\n", sizeof days2);
+    printf("days3Size= %zd bytes\n", sizeof days3);
+    // ======= [指定初始化器] =======
+    /**
+     * MONTH 1 has 31 days
+     * MONTH 2 has 28 days
+     * MONTH 3 has 0 days
+     * MONTH 4 has 0 days
+     * MONTH 5 has 0 days
+     * MONTH 6 has 5 days
+     * MONTH 7 has 0 days
+     * MONTH 8 has 0 days
+     * MONTH 9 has 8 days
+     * MONTH 10 has 0 days
+     * MONTH 11 has 0 days
+     * MONTH 12 has 0 days
+     * 指定数组下标的元素进行初始化值，其他未进行设置的为默认值
+     */
+    const int days4[MONTHS] = {31, 28, [5]=5, [8]=8};
+    for (int index = 0; index < sizeof days4 / sizeof days4[0]; index++) {
+        printf("MONTH %d has %d days\n", index + 1, days4[index]);
+    }
+    /**
+     * staff size = 7: 下标到6,也就是7个元素
+     * stuff size = 9: 下标到6之后,还有2个元素,也就是9个元素
+     */
+    int staff[] = {1, [6]=7};
+    int stuff[] = {1, [6]=7, 8, 9};
+    printf("staff size = %zd", sizeof staff / sizeof staff[0]);
+    printf("stuff size = %zd", sizeof stuff / sizeof stuff[0]);
+
+}
