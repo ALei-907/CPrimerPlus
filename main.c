c29cc5b303df1190272bde64e5b913fc260ef11f
leilimin <461083868@qq.com>
2022-07-31 22:18:33 +0800

    feat(C): 第11章
    1.通过数组与char指针来初始化字符串

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define MSG "i am a symbolic string constant."
-#define MAXLENGTH 81
+
 // 为何在此处声明,因为程序是由上至下执行的,如果不在此处声明,那执行至main()时就不知道print_hello()表示的含义
 // 1.声明函数原型
 void practice(void); /* help print "hello world"*/
@@ -14,18 +14,9 @@ int main() {
 
 // 3.函数定义
 void practice(void) {
-//    char words[MAXLENGTH] = "i am a string in a array.";
-    char words[MAXLENGTH] = "i am a string in a "
-                            "array.";
-
-    const char *pt1 = "something is pointing at me.";
-    puts("Here are some strings.");
-    puts(MSG);
-    puts(words);
-    puts(pt1);
-    words[8] = 'p';
-    puts(words);
-
+//    const char *ptChar = MSG;
+    char chArr[] = MSG;
+    printf("%c", *(++chArr));
 }
 
 
