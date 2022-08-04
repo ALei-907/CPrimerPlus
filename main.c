f41cf90a6e1c6463d3fc5413c724610d456d5160
leilimin <461083868@qq.com>
2022-08-04 23:28:03 +0800

    chapter(C): 12.1
    1.存储类别

--- main.c
+++ main.c
@@ -1,22 +1,22 @@
 #include "stdio.h"
 
 // 为何在此处声明,因为程序是由上至下执行的,如果不在此处声明,那执行至main()时就不知道print_hello()表示的含义
-int *practice(void);    /* 1.声明函数原型 */
+void practice(void);    /* 1.声明函数原型 */
 
-int a = 1;
+int COL = 3;
+int COL_STATIC = 4;
 
 int main() {
+    extern int COL;
+    extern int COL_STATIC;
+    printf("%d\n", COL);
+    printf("%d\n", COL_STATIC);
+
     /* 2.函数调用 */
-    int *pt = practice();
-    printf("Show Main block variable a.value=[%d],a.address=[%p]\n", *pt, pt);
+    practice();
     return 0;
 }
 
-int *practice(void) {   /* 3.函数定义 */
-    printf("external variable a.value=[%d],a.address=[%p]\n", a, &a);
-
-    auto int a = 2;
-    printf("block variable a.value=[%d],a.address=[%p]\n", a, &a);
-    return &a;
+void practice(void) {   /* 3.函数定义 */
 }
 
