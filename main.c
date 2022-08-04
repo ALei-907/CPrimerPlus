b8bab85f0f45d0584f7288c8e6fb685fe078fccc
leilimin <461083868@qq.com>
2022-08-04 13:58:50 +0800

    chapter(C): test auto variable

--- main.c
+++ main.c
@@ -1,24 +1,22 @@
 #include "stdio.h"
-#include "string.h"
 
 // 为何在此处声明,因为程序是由上至下执行的,如果不在此处声明,那执行至main()时就不知道print_hello()表示的含义
-void practice(void);    /* 1.声明函数原型 */
+int *practice(void);    /* 1.声明函数原型 */
+
+int a = 1;
 
 int main() {
-
-    practice();         /* 2.5函数调用 */
+    /* 2.函数调用 */
+    int *pt = practice();
+    printf("Show Main block variable a.value=[%d],a.address=[%p]\n", *pt, pt);
     return 0;
 }
 
-void practice(void) {   /* 3.函数定义 */
-    char chArr[] = "1111C";
-    puts(chArr);
+int *practice(void) {   /* 3.函数定义 */
+    printf("external variable a.value=[%d],a.address=[%p]\n", a, &a);
 
-    /**
-     * 语法错误: "1111"表示的是一个地址
-     *          数组的首地址是const常量是不允许改变的
-     */
-    strcpy(chArr, "11C");
-    puts(chArr);
+    auto int a = 2;
+    printf("block variable a.value=[%d],a.address=[%p]\n", a, &a);
+    return &a;
 }
 
